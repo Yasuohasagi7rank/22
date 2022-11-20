8cac51594ab337d5212b6d133651524be51d5524
Yasuohasagi7rank <112807477+Yasuohasagi7rank@users.noreply.github.com>
2022-11-21 01:14:29 +0300

    Add files via upload

---
+++ 7.60/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _7._60
+{
+    class Program
+    {
+        static int Metod(string Name, int Min = int.MinValue, int Max = int.MaxValue)
+        {
+            Console.WriteLine($"Введите {Name}:");
+            int Num;
+            while (!(int.TryParse(Console.ReadLine(), out Num) && Num >= Min && Num <= Max)) Console.WriteLine($"Неверно {Name}:");
+            return Num;
+        }
+        static void Main(string[] args)
+        {
+            Random R = new Random();
+            int a = Metod("количество квартир ");
+            int b = 0;
+            int c = 0;
+            for (int i = 1; i <= a; i++)
+            {
+                int temp = R.Next(1, 10);
+                if (temp >= c) { c = temp; b = i; }
+                Console.Write(temp + " ");
+            }
+            Console.WriteLine("\n" + b);
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
